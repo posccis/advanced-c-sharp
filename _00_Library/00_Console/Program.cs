99b2339cb6fcd6aca3530b238431463a79c55d52
Posccis <lkzq33@gmail.com>
2022-11-21 10:48:18 -0300

    new(main): First commit and working with serialization

---
+++ _00_Library/00_Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using _00_Library;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace _00_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario()
+            {
+                Nome="Victor",
+                CPF="555.555.555-55",
+                Email="lkzq33@gmail.com"
+            };
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Usuario));
+            StreamWriter stream = new StreamWriter(@"C:\Users\victo\OneDrive\Área de Trabalho\MAIN\_01_SerializadorXML.xml");
+            xmlSerializer.Serialize(stream, usuario);
+        }
+    }
+}
---
+++ _00_Library/_00_Library/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_Library
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+    }
+}
